d009a02dfc797d0d06d70113f32b46c4ab179f87
Ashley-23 <ashleysatchivi92@gmail.com>
2022-04-20 10:12:58 +0000

    First commit

---
+++ src/test/java/com/example/projet_mysql/ProjetMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

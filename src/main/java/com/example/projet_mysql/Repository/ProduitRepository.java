3e8908803e3df90614f079ad82ecc2dbde8be15b
Ashley-23 <ashleysatchivi92@gmail.com>
2022-04-20 12:03:45 +0000

    Modification de ProduitService

--- src/main/java/com/example/projet_mysql/Repository/ProduitRepository.java
+++ src/main/java/com/example/projet_mysql/Repository/ProduitRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProduitRepository extends JpaRepository <Produit,Integer>{
-
-
-}
+public interface ProduitRepository extends JpaRepository <Produit,Integer>{}
